88483f5a55704dc78642dbb8934c0a923cad7716
Tsavo Knott <tsavo@pieces.app>
2025-12-02 20:48:01 -0500

    fix: sort exports alphabetically

--- lib/runtime_common_codestyle.dart
+++ lib/runtime_common_codestyle.dart
@@ -2,8 +2,8 @@
 // Note: recommended.yaml is included via 'include: package:runtime_common_codestyle/recommended.yaml'
 // in analysis_options.yaml files, not via Dart exports
 export 'custom_lints/plugin.dart' show createPlugin;
+export 'custom_lints/prefer_for_in_over_foreach_lint.dart' show PreferForInOverForEachLint;
 export 'custom_lints/require_closure_parameter_types_lint.dart' show RequireClosureParameterTypesLint;
 export 'custom_lints/require_explicit_type_annotations_lint.dart' show RequireExplicitTypeAnnotationsLint;
 export 'custom_lints/require_show_hide_in_exports_lint.dart' show RequireShowHideInExportsLint;
 export 'custom_lints/unused_variable_lint.dart' show UnusedVariableLint;
-export 'custom_lints/prefer_for_in_over_foreach_lint.dart' show PreferForInOverForEachLint;
